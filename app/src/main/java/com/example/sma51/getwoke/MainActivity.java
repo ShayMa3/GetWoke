8035f30cd0ce9539e33baea4a3e232baf7375258
Shay Ma <s.ma5102@gmail.com>
2019-03-10 23:58:40 -0700

    timepicker

--- app/src/main/java/com/example/sma51/getwoke/MainActivity.java
+++ app/src/main/java/com/example/sma51/getwoke/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.sma51.getwoke;
 
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openPicker = (Button) findViewById(R.id.open_picker_button);
+        openPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        TextView timeText = (TextView) findViewById(R.id.time_text);
+        timeText.setText("Hour: " + hourOfDay + " | Minute: " + minute);
     }
 }

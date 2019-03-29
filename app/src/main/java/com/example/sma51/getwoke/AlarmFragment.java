36b943e2b9d82e50ab83406417e018da28da86ff
shayma4 <s.ma5102@gmail.com>
2019-03-29 14:13:09 -0700

    nav bar start

--- app/src/main/java/com/example/sma51/getwoke/AlarmFragment.java
+++ app/src/main/java/com/example/sma51/getwoke/AlarmFragment.java
@@ -1,17 +1,24 @@
 package com.example.sma51.getwoke;
 
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.Preconditions;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AlarmFragment extends Fragment {
+public class AlarmFragment extends Fragment implements TimePickerDialog.OnTimeSetListener{
 
 
     public AlarmFragment() {
@@ -22,8 +29,28 @@ public class AlarmFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+
+        // Inflate layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_alarm, container, false);
+
+
+        Button openPicker = (Button) rootView.findViewById(R.id.open_picker_button);
+        openPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getFragmentManager(), "time picker");
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        TextView timeText = (TextView) getView().findViewById(R.id.time_text);
+        timeText.setText(getString(R.string.time_text, hourOfDay, minute));
     }
 
 }
--- app/src/main/java/com/example/sma51/getwoke/HomeFragment.java
+++ app/src/main/java/com/example/sma51/getwoke/HomeFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +15,8 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private TextView noAlarmText;
+    private Button wakeButton;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +27,11 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Button wakeButton = (Button) rootView.findViewById(R.id.wake_up_button);
+        TextView noAlarmText = (TextView) rootView.findViewById(R.id.no_alarm_text);
+
+        return rootView;
     }
 
 }
